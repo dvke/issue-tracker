72884c0bc0a7de97d2224cf880df8487f08a02ce
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-01-27 13:29:00 +0100

    Refactor: Organize Imports

---
+++ app/components/index.tsx
@@ -0,0 +1,9 @@
+import ErrorMessage from "./ErrorMessage";
+import IssueStatusBadge from "./IssueStatusBadge";
+import Link from "./Link";
+import Spinner from "./Spinner";
+
+export { Link };
+export { ErrorMessage };
+export { IssueStatusBadge };
+export { Spinner };
