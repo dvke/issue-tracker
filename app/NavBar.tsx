1d1b49630bfe696c6d05e0b761c98b7471552c33
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-01-30 11:46:34 +0100

    Add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,16 +4,17 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBugSharp } from "react-icons/io5";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data } = useSession();
 
   const Links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
   ];
-
-  console.log(process.env.GOOGLE_CLIENT_ID);
 
   return (
     <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14 ">
@@ -22,19 +23,28 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {Links.map((link) => (
-          <Link
-            key={link.href}
-            className={classnames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href={"/api/auth/signout"}>Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href={"/api/auth/signin"}>Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
