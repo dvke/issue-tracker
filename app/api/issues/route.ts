13fca1843a23b6e3e753fec18dbb02ea612314be
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-02-24 19:34:47 +0100

    deployment issue in api/issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "../../validationSchemas";
 import { getServerSession } from "next-auth";
-import authOptions from "../../auth/authOptions";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -10,9 +10,8 @@ export async function POST(request: NextRequest) {
 
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
-
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
